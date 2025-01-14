a8024e7a21636b6ef7bd46475adaa1e6090f1056
Darek <34578369+darekbx@users.noreply.github.com>
2025-01-14 19:31:45 +0100

    Improved error box UI

--- app/src/main/java/com/darekbx/wheresmybus/ui/MapDroidScreen.kt
+++ app/src/main/java/com/darekbx/wheresmybus/ui/MapDroidScreen.kt
@@ -1,20 +1,16 @@
 package com.darekbx.wheresmybus.ui
 
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -25,14 +21,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.darekbx.wheresmybus.R
+import com.darekbx.wheresmybus.ui.theme.WheresMyBusTheme
 import com.darekbx.wheresmybus.utils.LineNumberCreator
 import com.darekbx.wheresmybus.viewmodel.BusStopsViewModel
 import org.koin.androidx.compose.koinViewModel
@@ -45,7 +44,6 @@ import org.osmdroid.views.overlay.Marker
  * TODO:
  *  - my location marker
  *  - progress bars - load lines, load live data
- *  - error handling (SuccessResult, FailureResult)
  *  - mark trams stops in different icon
  *  - bus stop icon
  *  - add support for trams
@@ -115,35 +113,58 @@ private fun LoadingBox(modifier: Modifier) {
     }
 }
 
-@Preview
 @Composable
 fun ErrorBox(error: Throwable = Throwable("Message"), onClose: () -> Unit = {}) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        contentAlignment = Alignment.Center
+    ) {
         Box(
             modifier = Modifier
-                .defaultCard()
-                .border(2.dp, Color(0xEEAA0000), RoundedCornerShape(8.dp))
                 .padding(8.dp)
-                .clip(RoundedCornerShape(8.dp)),
+                .shadow(8.dp)
+                .background(
+                    Color(0xFFFECCCD),
+                    RoundedCornerShape(8.dp)
+                )
+                .padding(16.dp),
             contentAlignment = Alignment.Center
         ) {
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_error),
-                        contentDescription = "Error",
-                        tint = Color(0xEEAA0000)
-                    )
-                    Spacer(Modifier.width(8.dp))
-                    Text(text = error.message ?: "Unknown error", fontSize = 16.sp)
-                }
+                Icon(
+                    modifier = Modifier.size(64.dp),
+                    painter = painterResource(id = R.drawable.ic_error),
+                    contentDescription = "Error",
+                    tint = Color(0xFFEF4433)
+                )
+                Text(
+                    text = error.message ?: "Unknown error",
+                    modifier = Modifier.padding(top = 8.dp),
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFFEF4433)
+                )
                 Text(
                     text = error.toString(),
-                    modifier = Modifier.padding(top = 16.dp, bottom = 16.dp),
+                    modifier = Modifier.padding(16.dp),
+                    textAlign = TextAlign.Center,
                     fontSize = 12.sp
                 )
                 Button(onClick = onClose) { Text("Close") }
             }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ErrorBoxPreview() {
+    WheresMyBusTheme {
+        Surface {
+            ErrorBox()
         }
     }
 }
--- app/src/test/java/com/darekbx/wheresmybus/ExampleUnitTest.kt
+++ app/src/test/java/com/darekbx/wheresmybus/ExampleUnitTest.kt
@@ -3,6 +3,8 @@ package com.darekbx.wheresmybus
 import org.junit.Test
 
 import org.junit.Assert.*
+import kotlin.system.measureNanoTime
+import kotlin.time.measureTime
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +12,42 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun addition_isCorrect1() {
+        val test = Test("aaa")
+        println(measureTime {
+            var a = 0
+            (0..100000).forEach {
+                val name = test::class
+                a += test.a.length
+            }
+        })
+    }
+
+    @Test
+    fun addition_isCorrect2() {
+        val test = Test("aaa")
+        println(measureTime {
+            var a = 0
+            (0..100000).forEach {
+                val name = test.javaClass.name
+                a += name.length
+            }
+        })
+    }
+
+    @org.junit.Test
+    fun addition_isCorrect3() {
+        val test = Test("aaa")
+        println(measureTime {
+            var a = 0
+            (0..100000).forEach {
+                val name = test.a
+                a += name.length
+            }
+        })
     }
 }
+
+data class Test(val a: String)
